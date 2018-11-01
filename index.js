3be9d22f20e95207b1de7e6327a3eaa907e4fb26
Dominggus Salampessy <dominggus@scentronix.com>
2018-11-01 14:38:17 +0100

    add log

--- index.js
+++ index.js
@@ -188,6 +188,7 @@ const query = async function(...args) {
           resetClient() // reset the client
           reject(err) // reject the promise with the error
         } else if (err) {
+          console.log('ERROR: ', JSON.stringify(err));
           if (this.rollback) {
             await query('ROLLBACK')
             this.rollback(err)
