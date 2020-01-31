7909f3feee7e4c2e8264a197ae8ea1e9bbf1f5b0
Jeremy Daly <jeremydaly@jeremydaly.com>
2020-01-31 14:55:27 -0500

    version bump

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  * More detail regarding the MySQL module can be found here:
  * https://github.com/mysqljs/mysql
  * @author Jeremy Daly <jeremy@jeremydaly.com>
- * @version 1.5.0
+ * @version 1.5.3
  * @license MIT
  */
 
